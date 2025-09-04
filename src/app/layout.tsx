5f638f488eefaac49a6b07088f4d03cedbe6e5b5
ndambodavid <linmik420@gmail.com>
2025-09-04 12:31:14 +0300

    added web speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
           <Analytics />
+          <SpeedInsights />
       </body>
     </html>
   );
