71a2903f7c8311f6d3856161d0bc1b1aa49a90a5
ndambodavid <linmik420@gmail.com>
2025-09-07 20:28:41 +0300

    updated time log module

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -2,15 +2,24 @@ import { SectionCards } from "@/components/dashboard/section-cards"
 import { ChartAreaInteractive } from "@/components/dashboard/chart-area-interactive"
 import { DataTable } from "@/components/dashboard/data-table"
 import data from "@/data/tasks.json"
+import { Button } from "@/components/ui/button"
+import { Task } from "@/types/task"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuPortal, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu"
+import { IconDotsVertical } from "@tabler/icons-react"
+import { ColumnDef } from "@tanstack/react-table"
 
 export default function DashboardPage() {
+
+  
+
   return (
-    <>
+    
+      <>
       <SectionCards />
       <div className="px-4 lg:px-6">
         <ChartAreaInteractive />
       </div>
-      <DataTable data={data} />
+      {/* <DataTable data={data} /> */}
     </>
   )
 }
