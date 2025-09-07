ecf87ca6658d6af565d429f0a8ae4d57606ad032
ndambodavid <linmik420@gmail.com>
2025-09-07 20:05:23 +0300

    updated time log module

--- src/app/(client)/client/page.tsx
+++ src/app/(client)/client/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { SectionCards } from "@/components/dashboard/section-cards"
 import { ChartAreaInteractive } from "@/components/dashboard/chart-area-interactive"
 import { DataTable } from "@/components/dashboard/data-table"
